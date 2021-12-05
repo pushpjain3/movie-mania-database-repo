638ae5c29aefb9d56bf7d93749812311624de4a6
Pushp Jain <77234511+pushpjain3@users.noreply.github.com>
2021-12-06 00:26:59 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA9BnlX96fMf7XiUVCFRsoQzG8DGERJkeY",
-  authDomain: "disneyplus-clone-a33d5.firebaseapp.com",
-  projectId: "disneyplus-clone-a33d5",
-  storageBucket: "disneyplus-clone-a33d5.appspot.com",
-  messagingSenderId: "37918794208",
-  appId: "1:37918794208:web:dbe9842dfe1dda522a4b85",
-  measurementId: "G-DRVLJKWRWG",
+  apiKey: "AIzaSyBGa03Jn-I3jTlU6j-mNydopF6fegpUEbg",
+  authDomain: "demo2-dd5ea.firebaseapp.com",
+  projectId: "demo2-dd5ea",
+  storageBucket: "demo2-dd5ea.appspot.com",
+  messagingSenderId: "772052618542",
+  appId: "1:772052618542:web:7cbb44bd1e7bd56329bebf",
+  measurementId: "G-LB5RNYKZH2",
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
